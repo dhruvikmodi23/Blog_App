caf62ad85d99fba85f9463f3ce074992c2bfdb2a
dhruvikmodi23 <dhruvikmodi23@gmail.com>
2024-09-11 23:20:11 +0530

    Debugging

--- Blog_App/src/main.jsx
+++ Blog_App/src/main.jsx
@@ -17,67 +17,70 @@ import Post from "./pages/Post";
 
 import AllPosts from "./pages/AllPosts";
 
-
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: [
       {
-        path: '/',
-        element: <Home />
+        path: "/",
+        element: <Home />,
       },
       {
-        path: '/login',
+        path: "/login",
         element: (
           <AuthLayout authentication={false}>
             <Login />
           </AuthLayout>
-        )
-
-      }
-    ]
+        ),
+      },
+      {
+        path: "/signup",
+        element: (
+          <AuthLayout authentication={false}>
+            <Signup />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/all-posts",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AllPosts />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/add-post",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AddPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/edit-post/:slug",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <EditPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/post/:slug",
+        element: <Post />,
+      },
+    ],
   },
-  {
-    path: "/all-posts",
-    element: (
-      <AuthLayout authentication>
-        {" "}
-        <AllPosts />
-      </AuthLayout>
-    ),
-  },
-  {
-    path: "/add-post",
-    element: (
-      <AuthLayout authentication>
-        {" "}
-        <AddPost />
-      </AuthLayout>
-    ),
-  },
-  {
-    path: "/edit-post/:slug",
-    element: (
-      <AuthLayout authentication>
-        {" "}
-        <EditPost />
-      </AuthLayout>
-    ),
-  },
-  {
-    path: "/post/:slug",
-    element: <Post />,
-  },
-
-
-
 ])
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
-  </StrictMode>,
+  </React.StrictMode>,
 )
